81661768b684db665a16d30d4d60273507b0099e
syedasariya2 <twinklingwatermelon8@gmail.com>
2026-09-14 13:46:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.set_page_config(
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 TOKENIZER_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-LLM_MODEL = "llama-3.3-70b-versatile"
+LLM_MODEL = "openai/gpt-oss-120b"
 
 CHUNK_SIZE = 350       # tokens
 CHUNK_OVERLAP = 60     # tokens
